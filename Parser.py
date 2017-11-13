138fbb00ef3c4de94060b784b384de1b7c4a7149
Alexander Morsey <alexmorsey@gmail.com>
2017-11-13 14:28:21 -0800

    Makeing style changes. Does not run.

--- Parser.py
+++ Parser.py
@@ -2,19 +2,21 @@ from selenium import webdriver
 from time import sleep
 from bs4 import BeautifulSoup
 
-def copyData():
+def copy_data(dictionary):
     try:
+        # Find and extract HTML from popup window.
         html = browser.find_element_by_class_name("tab-ubertipTooltip")\
         .get_attribute('innerHTML')
+        # Parse out lines with key infromation.
         soup = BeautifulSoup(html, "html.parser")
-        l = soup.find_all('span')
-        for i in range(3,12,2):
-            f.write(l[i].text + l[i+1].text + "\n")
-        f.write("\n")
+        spanElements = soup.find_all('span')
+
+
+        
     except:
         pass
 
-def scrapeTable():
+def scrape_table():
     browser = webdriver.Firefox() 
     url = ("https://public.tableau.com/views/DataReaper69-MatchupWinRat"
            "es/Dashboard1?:embed=y&:showVizHome=no&:host_url=https%3A%2"
@@ -22,21 +24,17 @@ def scrapeTable():
            "oolbar=yes&:animate_transition=yes&:display_static_image=no"
            "&:display_spinner=no&:display_overlay=yes&:display_count=ye"
            "s&publish=yes&:loadOrderID=0")
-
     browser.get(url)
     f = open('Data.txt', 'w')
-
     el = browser.find_element_by_class_name("tvScrollContainer")
     browser.execute_script("window.scrollTo(0, 160);")
-
+    classMatchup = {}
     for j in range(22):
         for i in range(21):
             act = webdriver.ActionChains(browser)
             act.move_to_element_with_offset(el,2+23*i,2+j*31)
             act.perform()
-            copyData()
-
-    # Close everything
+            copyData(classMatchup)
     f.close()
     browser.close()
 
