8bc254ff9ec4bc752d48dd989efa3b38c4de4037
Alexander Morsey <alexmorsey@gmail.com>
2017-11-13 17:35:21 -0800

    Tidying up

--- Parser.py
+++ Parser.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from time import sleep
 from bs4 import BeautifulSoup
 
 def copy_data(deck_stats):
@@ -29,26 +28,25 @@ def scrape_table():
     browser = webdriver.Firefox()
     browser.get(url)
     # The browser needs to scroll down to see the whole table.
-    # This issue will vary dependant on your screen.
+    # This issue will vary dependent on your screen.
     el = browser.find_element_by_class_name("tvScrollContainer")
     browser.execute_script("window.scrollTo(0, 160);")
     deck_stats = {}
     for j in range(22):
         for i in range(21):
-            # It is importnat to create a new action chain for every
+            # It is importnat to create a new ActionChains for every
             # operation otherwise each new iteration will perform ALL
             # previous commands.
             act = webdriver.ActionChains(browser)
             act.move_to_element_with_offset(el,2+23*i,2+j*31)
             act.perform()
             copyData(deck_stats)
-
-    
     browser.close()
-
+    return deck_stats
 
 if __name__ == "__main__":
-    scrape_table()
+    deck_stats = scrape_table()
+    # Add write operation.
 
 
 
