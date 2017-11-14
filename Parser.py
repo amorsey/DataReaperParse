d13a45bf7916aa84a7a59a38615fef5f409fa196
Alexander Morsey <alexmorsey@gmail.com>
2017-11-14 09:48:37 -0800

    Data now is in JSON formatting.

--- Parser.py
+++ Parser.py
@@ -1,22 +1,26 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import json
 
-def copy_data(deck_stats):
-    # Although a hack, try/except seems to be the best way to deal with
-    # empty cells that create no pop-up.
-    try:
-        # Find and extract HTML from popup window.
-        html = browser.find_element_by_class_name("tab-ubertipTooltip")\
-        .get_attribute('innerHTML')
-        # Parse out key infromation.
-        soup = BeautifulSoup(html, "html.parser")
-        text_elements = soup.find_all('span')
-        deck_stats[text_elements[2]] = {text_elements[3]:{
-                                        "Games":text_elements[5]
-                                        "Winrate":text_elements[7],
-                                        "SDWinrate":text_elements[9],}}
-    except:
-        pass
+def copy_data(html, deck_stats):
+    # Parse out key infromation.
+    soup = BeautifulSoup(html, "html.parser")
+    text_elements = soup.find_all('span')
+    deck_name = text_elements[4].text
+    if deck_name not in deck_stats:
+        deck_stats[deck_name] = [{
+                                      "Opponent":text_elements[6].text,
+                                      "Games":text_elements[8].text,
+                                      "Winrate":text_elements[10].text,
+                                      "SDWinrate":text_elements[12].text,}]
+    else:
+        deck_stats[deck_name].append(
+                                        {"Opponent":text_elements[6].text,
+                                         "Games":text_elements[8].text,
+                                         "Winrate":text_elements[10].text,
+                                         "SDWinrate":text_elements[12].text,
+                                        })
+
 
 def scrape_table():
     url = ("https://public.tableau.com/views/DataReaper69-MatchupWinRat"
@@ -32,21 +36,29 @@ def scrape_table():
     el = browser.find_element_by_class_name("tvScrollContainer")
     browser.execute_script("window.scrollTo(0, 160);")
     deck_stats = {}
-    for j in range(22):
+    for j in range(3):
         for i in range(21):
             # It is importnat to create a new ActionChains for every
             # operation otherwise each new iteration will perform ALL
             # previous commands.
+            x_offset = 2+23*i
+            y_offset = 2+j*31
             act = webdriver.ActionChains(browser)
-            act.move_to_element_with_offset(el,2+23*i,2+j*31)
+            act.move_to_element_with_offset(el,x_offset,y_offset)
             act.perform()
-            copyData(deck_stats)
+            try:
+                html = browser.find_element_by_class_name("tab-ubertipTooltip").get_attribute('innerHTML')
+            except:
+                html = ""
+            if html != "":
+                copy_data(html, deck_stats)
     browser.close()
     return deck_stats
 
 if __name__ == "__main__":
     deck_stats = scrape_table()
-    # Add write operation.
+    with open('data.json', 'w') as fp:
+        json.dump(deck_stats, fp, sort_keys=True, indent=4)
 
 
 
